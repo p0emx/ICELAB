d02682aed6b27ef9d7b466063ca0ac238eba2fa5
peppermint <69010007@kmitl.ac.th>
2026-09-23 17:10:22 +0700

    feature

--- Hello.c
+++ Hello.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+int greeting(const char *name);
 int main(void)
 {
-printf("Hello, Git Collaboration!\n");
-return 0;
+    greeting("Developer A,B,C !!");
+    return 0;
+}
+
+int greeting(const char *name)
+{
+    printf("Hello, %s!\n", name);
+    return 0;
 }
