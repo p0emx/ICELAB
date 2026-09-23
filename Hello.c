89dac9f91d04326bef6a6f32a31dbfecf3de5ba1
peppermint <69010007@kmitl.ac.th>
2026-09-23 17:21:23 +0700

    already fix

--- Hello.c
+++ Hello.c
@@ -5,7 +5,7 @@ int main(void)
     greeting("Developer A,B,C !!");
     return 0;
 }
-
+// print a greeting message for the specified user
 int greeting(const char *name)
 {
     printf("Hello, %s!\n", name);
